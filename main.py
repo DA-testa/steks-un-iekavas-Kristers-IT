1c7e8376e68cdb30082230db758582e755585d0e
Kristers Vilks <112978132+Kristers-IT@users.noreply.github.com>
2023-02-11 17:32:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def find_mismatch(text):
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            if opening_brackets_stack == [] or not are_matching.(opening_brackets_stack[-1].char,next):
+            if opening_brackets_stack == [] or not are_matching(opening_brackets_stack[-1].char, next):
                 print(position + 1);
                 return 0;
             opening_brackets_stack.pop();
